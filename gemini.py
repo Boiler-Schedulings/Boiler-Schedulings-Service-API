4830ad371e138bcfa4e261052c12f3b33233ee1c
Kevin Huang <kevinhuang2011@gmail.com>
2024-01-20 19:07:44 +0000

    Further prompt engineering

--- gemini.py
+++ gemini.py
@@ -1,20 +1,21 @@
 import google.generativeai as genai
 from vectordb import process_data_and_query
 
-genai.configure(api_key='Key')
+genai.configure(api_key='AIzaSyCCrHwe8X7xKJYKpqQhJwBOqaMqB8R-GW8')
 model = genai.GenerativeModel('gemini-pro')
 
 data_dir = "data/course_docs_emb.json"
 query_texts = ["I want a course about the software development working with a team."]
-n_results = 2
+n_results = 6
 courses = process_data_and_query(data_dir, query_texts, n_results)
 
-initial_prompt_1 = "You are an assistant helping students register for classes based on their degree requirements and interests. The student is currently studying"
+initial_prompt_1 = "You are an assistant helping talking to students directly to register for classes based on their degree requirements and interests. The student is currently studying:"
 degrees = "Computer Science, Data Science"
-initial_prompt_2 = ". Here are a some relevant courses that may be suitable for the student:"
+initial_prompt_2 = ". For your reference, are a some relevant courses that may be suitable for the student:"
 course_prompt = "\n".join(courses)
+initial_prompt_3 = ". Please concisely describe a couple courses that you think would be the best fit for the student and why."
 
-final_prompt = initial_prompt_1 + degrees + initial_prompt_2 + course_prompt
+final_prompt = initial_prompt_1 + degrees + initial_prompt_2 + course_prompt + initial_prompt_3
 
 response = model.generate_content(final_prompt)
 
