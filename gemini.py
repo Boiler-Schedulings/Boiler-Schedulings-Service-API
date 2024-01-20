804b22c63f43a4366564852641674fab34435f7b
Kevin Huang <kevinhuang2011@gmail.com>
2024-01-20 18:58:11 +0000

    Integrate chroma's db lookup into Gemini

--- gemini.py
+++ gemini.py
@@ -1,11 +1,21 @@
-#pip install -q -U google-generativeai
-
 import google.generativeai as genai
+from vectordb import process_data_and_query
 
-genai.configure(api_key='API_KEY')
-
+genai.configure(api_key='Key')
 model = genai.GenerativeModel('gemini-pro')
 
-response = model.generate_content("What is the meaning of life?")
+data_dir = "data/course_docs_emb.json"
+query_texts = ["I want a course about the software development working with a team."]
+n_results = 2
+courses = process_data_and_query(data_dir, query_texts, n_results)
+
+initial_prompt_1 = "You are an assistant helping students register for classes based on their degree requirements and interests. The student is currently studying"
+degrees = "Computer Science, Data Science"
+initial_prompt_2 = ". Here are a some relevant courses that may be suitable for the student:"
+course_prompt = "\n".join(courses)
+
+final_prompt = initial_prompt_1 + degrees + initial_prompt_2 + course_prompt
+
+response = model.generate_content(final_prompt)
 
 print(response.text)
