44aeefcc0b4ce47eab3f2bf01b39593fcac4925a
Kevin Huang <kevinhuang2011@gmail.com>
2024-01-20 20:13:13 +0000

    Add API key

--- gemini.py
+++ gemini.py
@@ -10,7 +10,7 @@ n_results = 6
 courses = process_data_and_query(data_dir, query_texts, n_results)
 
 initial_prompt_1 = "You are an assistant chat bot helping students to register for classes based on their degree requirements and interests. The student is currently studying:"
-degrees = "Computer Science, Data Science"
+degrees = "Computer Science"
 initial_prompt_2 = ". For your reference, are a some relevant courses that may be suitable for the student:"
 course_prompt = "\n".join(courses)
 initial_prompt_3 = ". Please speak directly to them and concisely describe a couple courses that you think would be the best fit for the student and why. Include all relevant information such as course name,code, credit hours, and brief description."
